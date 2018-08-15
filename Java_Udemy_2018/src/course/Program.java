21745ea715eb3039e57fb7f7c4c6ec673e206367
Jean Carvalho Silva <jean.csilva@kroton.com.br>
2018-08-15 17:50:32 -0300

    Alteracoes

--- Java_Udemy_2018/src/course/Program.java
+++ Java_Udemy_2018/src/course/Program.java
@@ -8,6 +8,17 @@ public class Program {
 
         Scanner sc = new Scanner(System.in);
 
+        byte x = -126;
+        String name = "Maria Antônia";
+        Object obj1 = "José Souza";
+        Object obj2 = 4.5f;
+
+        System.out.println(name);
+        System.out.println(obj1);
+        System.out.println(obj2);
+        System.out.println(x);
+
         System.out.println("Hello World");
+
     }
 }
